2390d1cf2eced095fca78ec3ed2fbc2e7a173b2d
Cole Challand <ctchalland@gmail.com>
2019-05-27 11:19:46 -0500

    Added submit method to post to the database

--- public/index.js
+++ public/index.js
@@ -1,19 +1,36 @@
 /* global Vue, VueRouter, axios */
 
-var HomePage = {
-  template: "#home-page",
+var RewardsPage = {
+  template: "#rewards-page",
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      email: "",
+      password: "",
+      errors: []
     };
   },
   created: function() {},
-  methods: {},
+  methods: {
+    submit: function() {
+      let params = {
+        email: this.email,
+        password: this.password
+      };
+      axios
+        .post("/users", params)
+        .then(function(response) {
+          router.push("/")
+        })
+        .catch(function(error) {
+          this.errors = error.response.data.errors;
+        }.bind(this));
+    }
+  },
   computed: {}
 };
 
 var router = new VueRouter({
-  routes: [{ path: "/", component: HomePage }],
+  routes: [{ path: "/", component: RewardsPage }],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
   }
