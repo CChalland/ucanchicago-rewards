71aa74ae7481c569b4f10128c4e3d1d61d41575a
Cole Challand <ctchalland@gmail.com>
2019-05-27 12:25:22 -0500

    Frontend for logging in

--- public/index.js
+++ public/index.js
@@ -29,8 +29,56 @@ var RewardsPage = {
   computed: {}
 };
 
+var LoginPage = {
+  template: "#login-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  created: function() {},
+  methods: {
+    submit: function() {
+      var params = {
+        auth: { email: this.email, password: this.password }
+      };
+      axios
+        .post("/user_token", params)
+        .then(function(response) {
+          axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          router.push("/");
+        })
+        .catch(function(error) {
+          this.errors = ["Invalid email or password."];
+          this.email = "";
+          this.password = "";
+        }.bind(this));
+    }
+  },
+  computed: {}
+};
+
+var ShowPage = {
+  template: "#show-page",
+  data: function() {
+    return {
+      message: "Welcome to Show!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
 var router = new VueRouter({
-  routes: [{ path: "/", component: RewardsPage }],
+  routes: [
+    { path: "/", component: RewardsPage },
+    { path: "/login", component: LoginPage },
+    { path: "/pwned", component: ShowPage }
+  ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
   }
