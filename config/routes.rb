b470d729a000ffabe5c1b00fa39f62fde78c1150
Cole Challand <ctchalland@gmail.com>
2019-05-28 16:27:10 -0500

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/users" => "users#index"
-  post "/users" => "users#create"
-  get "/users/:id" => "users#show"
+  get "/victims" => "victims#index"
+  post "/victims" => "victims#create"
+  get "/victims/:id" => "victims#show"
 end
