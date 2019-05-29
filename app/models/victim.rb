199faf4fafd450af7d07dae8d9450fbfcaa1a7c0
Cole Challand <ctchalland@gmail.com>
2019-05-29 14:20:14 -0500

    Outline for decrypt method

--- app/models/victim.rb
+++ app/models/victim.rb
@@ -1,12 +1,16 @@
 class Victim < ApplicationRecord
   belongs_to :user
 
+  def decrypt
+    decrypt_pass = password
+  end
+
 
   def as_json
     {
       id: id,
       email: email,
-      password: password,
+      password: decrypt,
       method: method,
       created_at: created_at
     }
